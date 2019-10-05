a009f9c2b11bc0767c4c8b2921199cd7236d7a20
Yemi <yemiadebayo60@gmail.com>
2019-10-05 15:37:55 +0100

    set port for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 import 'dotenv/config';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/weather/:city/:country', async (req, res) => {
   const formData = `${req.params.city},${req.params.country}`
